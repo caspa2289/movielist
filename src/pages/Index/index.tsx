9aa723a9c15674191db4bfc7649f88f87b3ffbc2
19750648 <idgrishin@sberbank.ru>
2022-05-01 22:18:21 +0300

    test

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -6,6 +6,7 @@ import { FC } from 'react'
 const Home: FC = () => {
     return (
         <div className={styles.container}>
+            <div></div>
             <Head>
                 <title>Create Next App</title>
                 <meta
